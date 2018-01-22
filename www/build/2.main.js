8cb5206ef0249a0517fc89ad39079da5ddaad06d
Denislav Popov <denislavpopov46@gmail.com>
2018-01-22 16:57:59 +0200

    Added: --- Performance optimizations for searchbar animations in Home page

--- www/build/2.main.js
+++ www/build/2.main.js
@@ -1,12 +1,12 @@
 webpackJsonp([2],{
 
-/***/ 287:
+/***/ 288:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(10);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(11);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__log_in__ = __webpack_require__(109);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "LogInPageModule", function() { return LogInPageModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
--- www/build/4.main.js
+++ www/build/4.main.js
@@ -1,12 +1,12 @@
 webpackJsonp([4],{
 
-/***/ 285:
+/***/ 286:
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
 Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(10);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1_ionic_angular__ = __webpack_require__(11);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__author__ = __webpack_require__(52);
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AuthorPageModule", function() { return AuthorPageModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
